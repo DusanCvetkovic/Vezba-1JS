3517059144e00c2e680db2a0ee4bb5ce1d3ba136
DusanCvetkovic <71986600+DusanCvetkovic@users.noreply.github.com>
2021-02-09 21:48:14 +0100

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,16 @@
+const name = 'Dusan';
+const age = 24;
+const isGood = true;
+const rating =3.5;
+const y= null;
+const x = undefined;
+let z;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isGood);
+console.log(typeof rating);
+console.log(typeof y);
+console.log(typeof x);
+console.log(typeof z);
+
